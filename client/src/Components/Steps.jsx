91036ef47a6e537d1b214fe3d2fd531df86af84b
sohaib shoukat <sohaibshoukat94@gmail.com>
2024-02-13 20:47:43 +0500

    push

--- client/src/Components/Steps.jsx
+++ client/src/Components/Steps.jsx
@@ -3,31 +3,31 @@ import BannerStep from "../assets/StepsBanner.jpg"
 
 const Steps = () => {
     return (
-        <div className='lg:w-[80%] md:w-[90%] w-[95%] m-auto py-32'>
-            <div className="flex md:flex-row flex-col gap-20 items-center justify-between">
+        <div className='lg:w-[80%] md:w-[90%] w-[90%] m-auto py-20 md:py-32'>
+            <div className="flex md:flex-row flex-col gap-20 lg:items-start xl:items-center justify-between">
                 <div className='basis-[40%]'>
                     <div className='flex flex-col gap-8'>
-                        <div className='text-left pb-20 border-b-2 border-black'>
-                            <h1 className='text-lg md:text-3xl font-bold mb-5'>Step 1</h1>
-                            <p className='text-gray-700 text-xl mb-5'>SignUp to Our Outstanding Platform.</p>
+                        <div className='text-left lg:pb-10 xl:pb-20 border-b-2 border-black'>
+                            <h1 className='text-lg md:text-3xl font-bold mb-2 md:mb-5'>Step 1</h1>
+                            <p className='text-gray-700 text-base md:text-xl mb-5'>SignUp to Our Outstanding Platform.</p>
                         </div>
-                        <div className='text-left pb-20 border-b-2 border-black'>
-                            <h1 className='text-lg md:text-3xl font-bold'>Step 2</h1>
-                            <p className='text-gray-700 text-xl mb-5'>You will assign with a dedicated panel to make your perzionlized Wedding invitation with our pre created templates.</p>
+                        <div className='text-left lg:pb-10 xl:pb-20 border-b-2 border-black'>
+                            <h1 className='text-lg md:text-3xl font-bold mb-2 md:mb-5'>Step 2</h1>
+                            <p className='text-gray-700 text-base md:text-xl mb-5'>You will assign with a dedicated panel to make your perzionlized Wedding invitation with our pre created templates.</p>
                         </div>
-                        <div className='text-left pb-20 border-b-2 border-black'>
-                            <h1 className='text-lg md:text-3xl font-bold'>Step 3</h1>
-                            <p className='text-gray-700 text-xl mb-5'>Share your invitation to all your guest and impress them with your invite.</p>
+                        <div className='text-left lg:pb-10 xl:pb-20 border-b-2 border-black'>
+                            <h1 className='text-lg md:text-3xl font-bold mb-2 md:mb-5'>Step 3</h1>
+                            <p className='text-gray-700 text-base md:text-xl mb-5'>Share your invitation to all your guest and impress them with your invite.</p>
                         </div>
                     </div>
                 </div>
                 <div className='basis-[50%] w-[100%]'>
                     <div className='mb-8 w-[100%]'>
-                        <img src={BannerStep} alt="" className='w-[70%] object-cover' />
+                        <img src={BannerStep} alt="" className='lg:w-[90%] xl:w-[70%] object-cover' />
                     </div>
-                    <h1 className='text-6xl font-semibold mb-5' style={{ lineHeight: "4.5rem" }}>How Everything <br /> Work</h1>
-                    <p className='max-w-[80%] text-gray-700 text-xl mb-5'>Create Memorable Events in 3 Easy Steps</p>
-          <button className='bg-amber-700 hover:bg-transparent border-amber-700 border-2 hover:text-amber-700 ease-in-out duration-300 text-white font-medium text-lg px-6 py-2 rounded-2xl'>Sign Up now</button>
+                    <h1 className='text-2xl md:text-3xl lg:text-4xl xl:text-6xl font-semibold mb-5 lg:leading-[2.5rem] xl:leading-[4.5rem]'>How Everything <br /> Work</h1>
+                    <p className='max-w-[80%] text-gray-700 text-lg md:text-xl mb-2 md:mb-5'>Create Memorable Events in 3 Easy Steps</p>
+          <button className='bg-amber-700 hover:bg-transparent border-amber-700 border-2 hover:text-amber-700 ease-in-out duration-300 text-white font-medium text-base md:text-lg px-4 py-2 md:px-6 md:py-2 rounded-2xl'>Sign Up now</button>
                 </div>
             </div>
         </div>
--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -10,11 +10,11 @@ import Steps from '../Components/Steps';
 const Home = () => {
   return (
     <>
-      <div className='flex flex-col md:flex-row justify-between gap-6 md:px-24 lg:px-36 py-12 m-auto mb-10'>
+      <div className='flex flex-col md:flex-row justify-between gap-6 px-8 md:px-14 lg:px-20 xl:px-36 py-12 m-auto mb-10'>
         <div className='text-left basis-[45%]'>
-          <h1 className='text-6xl font-semibold mb-5' style={{ lineHeight: "4.5rem" }}>Your Heartfelt Invitation <br /> Now on Your <br />Guest's Screens</h1>
-          <p className='max-w-[80%] text-gray-700 text-xl mb-5'>Infuse your wedding invitation with a personal touch that transcends the miles and brings the celebration straight to your guests' screens.</p>
-          <button className='bg-amber-700 hover:bg-transparent border-amber-700 border-2 hover:text-amber-700 ease-in-out duration-300 text-white font-medium text-lg px-6 py-2 rounded-2xl'>Try Now</button>
+          <h1 className='text-2xl md:text-3xl lg:text-4xl xl:text-6xl font-semibold mb-2 md:mb-5 lg:leading-[2.5rem] xl:leading-[4.5rem]'>Your Heartfelt Invitation <br /> Now on Your <br />Guest's Screens</h1>
+          <p className='xl:max-w-[80%] text-gray-700 text-base md:text-lg lg:text-xl mb-5'>Infuse your wedding invitation with a personal touch that transcends the miles and brings the celebration straight to your guests' screens.</p>
+          <button className='bg-amber-700 hover:bg-transparent border-amber-700 border-2 hover:text-amber-700 ease-in-out duration-300 text-white font-medium text-lg px-4 py-2 md:px-6 md:py-2 rounded-2xl'>Try Now</button>
 
         </div>
         <div className='basis-[45%] rounded-xl'>
@@ -23,14 +23,14 @@ const Home = () => {
       </div>
 
       {/*  */}
-      <div className='flex flex-col md:flex-row justify-between items-center gap-6 md:px-24 lg:px-36 py-28 m-auto bg-gray-100 '>
+      <div className='flex flex-col md:flex-row justify-between gap-6 px-8 md:px-14 lg:px-20 xl:px-36 py-12 m-auto bg-gray-100 '>
         <div className='basis-[45%] rounded-xl'>
           <img src={Invite} alt="" className='w-[100%] h-[100%] rounded-2xl object-cover' />
         </div>
         <div className='text-left basis-[45%]'>
-          <h1 className='text-6xl font-semibold mb-5' style={{ lineHeight: "4.5rem" }}>Unveiling <br />Digital Inviter</h1>
-          <p className='max-w-[80%] text-gray-700 text-xl mb-5'>Create A Custom Digital Invitation For Your Precious life Moments.</p>
-          <button className='bg-amber-700 hover:bg-transparent border-amber-700 border-2 hover:text-amber-700 ease-in-out duration-300 text-white font-medium text-lg px-6 py-2 rounded-2xl'>Demo</button>
+          <h1 className='text-2xl md:text-3xl lg:text-4xl xl:text-6xl font-semibold mb-2 md:mb-5 lg:leading-[2.5rem] xl:leading-[4.5rem]'>Unveiling <br />Digital Inviter</h1>
+          <p className='xl:max-w-[80%] text-gray-700 text-base md:text-lg lg:text-xl mb-5'>Create A Custom Digital Invitation For Your Precious life Moments.</p>
+          <button className='bg-amber-700 hover:bg-transparent border-amber-700 border-2 hover:text-amber-700 ease-in-out duration-300 text-white font-medium text-lg px-4 py-2 md:px-6 md:py-2 rounded-2xl'>Demo</button>
         </div>
       </div>
 
@@ -38,26 +38,26 @@ const Home = () => {
       {/*  */}
       <Steps/>
 
-      <div className='flex flex-col md:flex-row justify-between items-center gap-6 md:px-24 lg:px-36 py-28 m-auto bg-gray-100 '>
+      <div className='flex flex-col md:flex-row justify-between gap-6 px-8 md:px-14 lg:px-20 xl:px-36 py-12 m-auto bg-gray-100 '>
         <div className='basis-[45%] rounded-xl'>
           <img src={BannerInfo} alt="" className='w-[100%] h-[100%] rounded-2xl object-cover' />
         </div>
         <div className='text-left basis-[45%]'>
-          <p className='text-base text-gray-700 mb-20'>Cost-Effective</p>
-          <h1 className='text-6xl font-semibold mb-5' style={{ lineHeight: "4.5rem" }}>Celebrate Smart with <br />Digital Inviter</h1>
-          <p className='max-w-[80%] text-gray-700 text-xl mb-5'>Design luxurious invites with Einvite and save. Elegant, cost-effective, and just a click away to start your eco-friendly celebration journey.</p>
-          <button className='bg-amber-700 hover:bg-transparent border-amber-700 border-2 hover:text-amber-700 ease-in-out duration-300 text-white font-medium text-lg px-6 py-2 rounded-2xl'>Create Invite</button>
+          <p className='text-base text-gray-700 mb-5 md:mb-20'>Cost-Effective</p>
+          <h1 className='text-2xl md:text-3xl lg:text-4xl xl:text-6xl font-semibold mb-2 md:mb-5 lg:leading-[2.5rem] xl:leading-[4.5rem]'>Celebrate Smart with <br />Digital Inviter</h1>
+          <p className='xl:max-w-[80%] text-gray-700 text-base md:text-lg lg:text-xl mb-5'>Design luxurious invites with Einvite and save. Elegant, cost-effective, and just a click away to start your eco-friendly celebration journey.</p>
+          <button className='bg-amber-700 hover:bg-transparent border-amber-700 border-2 hover:text-amber-700 ease-in-out duration-300 text-white font-medium text-lg px-4 py-2 md:px-6 md:py-2 rounded-2xl'>Create Invite</button>
         </div>
       </div>
 
       {/*  */}
-      <div className="h-[100vh] bg-primary/30 py-32 text-center lg:max-w-[80%] md:max-w-[90%] max-w-[95%] m-auto">
+      <div className=" bg-primary/30 py-10 md:py-16 lg:py-20  xl:py-32 text-center lg:max-w-[85%] xl:max-w-[80%] md:max-w-[90%] max-w-[95%] m-auto">
         <div className="container mx-auto h-full flex flex-col justify-center">
           <motion.h2
             initial="hidden"
             animate="show"
             exit="hidden"
-            className="mb-8 xl:mb-0 lg:text-6xl text-black font-semibold"
+            className="mb-8 xl:mb-0  text-2xl md:text-4xl lg:text-6xl text-black font-semibold"
           >
             What clients <span className="text-amber-700">say.</span>
           </motion.h2>
@@ -73,12 +73,12 @@ const Home = () => {
         </div>
       </div>
 
-      <div className='flex flex-col md:flex-row justify-between items-center gap-6 md:px-24 lg:px-36 py-28 m-auto bg-gray-100 '>
+      <div className='flex flex-col md:flex-row justify-between gap-6 px-8 md:px-14 lg:px-20 xl:px-36 py-12 m-auto bg-gray-100 '>
         <div className='text-left basis-[55%]'>
-          <p className='text-base text-gray-700 mb-20'>- Free</p>
-          <h1 className='text-6xl font-semibold mb-5' style={{ lineHeight: "4.5rem" }}>Ready to Save Big  <br />on Stylish Invitations?</h1>
-          <p className='max-w-[90%] text-gray-700 text-xl mb-5'>Choose our environment and budget-friendly option today and begin crafting memorable moments with Einvite.</p>
-          <button className='bg-amber-700 hover:bg-transparent border-amber-700 border-2 hover:text-amber-700 ease-in-out duration-300 text-white font-medium text-lg px-6 py-2 rounded-2xl'>Create Invite</button>
+          <p className='text-base text-gray-700 mb-5 md:mb-20'>- Free</p>
+          <h1 className='text-2xl md:text-3xl lg:text-4xl xl:text-6xl font-semibold mb-2 md:mb-5 lg:leading-[2.5rem] xl:leading-[4.5rem]'>Ready to Save Big  <br />on Stylish Invitations?</h1>
+          <p className='xl:max-w-[80%] text-gray-700 text-base md:text-lg lg:text-xl mb-5'>Choose our environment and budget-friendly option today and begin crafting memorable moments with Einvite.</p>
+          <button className='bg-amber-700 hover:bg-transparent border-amber-700 border-2 hover:text-amber-700 ease-in-out duration-300 text-white font-medium text-lg px-4 py-2 md:px-6 md:py-2 rounded-2xl'>Create Invite</button>
         </div>
         <div className='basis-[40%] rounded-xl'>
           <img src={Banner4} alt="" className='w-[100%] h-[100%] rounded-2xl object-cover' />
