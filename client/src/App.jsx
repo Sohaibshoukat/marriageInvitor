286a46c1452edeb998f1ae19e0d9bb00762f3fd9
sohaib shoukat <sohaibshoukat94@gmail.com>
2024-02-17 13:22:47 +0500

    push

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,18 +7,18 @@ function App() {
 
   return (
     <>
-                <BrowserRouter>
-            <Nav />
-            {/* <Header /> */}
-            <Routes>
-              <Route path="/" element={<Home />}></Route>
-              {/* <Route path="/about" element={<About />}></Route>
+      <BrowserRouter>
+        <Nav />
+        {/* <Header /> */}
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          {/* <Route path="/about" element={<About />}></Route>
               <Route path="/contact" element={<Contact />}></Route>
               <Route path="/services" element={<Service />}></Route>
               <Route path="/testimonials" element={<Testimonial />}></Route>
               <Route path="/work" element={<Work />}></Route> */}
-            </Routes>
-          </BrowserRouter>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
